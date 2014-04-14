7615a72d907039120f3a3ac7c9bde20c96178a6e
benoitc <bchesneau@gmail.com>
2014-04-14 02:11:55 +0200

    reindent

--- src/idna.erl
+++ src/idna.erl
@@ -4,25 +4,23 @@
 
 -define(ACE_PREFIX, "xn--").
 
-
-
 to_ascii(Domain) ->
-  to_ascii(string:tokens(idna_unicode:downcase(Domain), "."), []).
+    to_ascii(string:tokens(idna_unicode:downcase(Domain), "."), []).
 
 utf8_to_ascii(Domain) ->
-  to_ascii(idna_ucs:from_utf8(Domain)).
+    to_ascii(idna_ucs:from_utf8(Domain)).
 
 to_ascii([], Acc) ->
-  lists:reverse(Acc);
+    lists:reverse(Acc);
 to_ascii([Label|Labels], []) ->
-  to_ascii(Labels, lists:reverse(label_to_ascii(Label)));
+    to_ascii(Labels, lists:reverse(label_to_ascii(Label)));
 to_ascii([Label|Labels], Acc) ->
-  to_ascii(Labels, lists:reverse(label_to_ascii(Label), [$.|Acc])).
+    to_ascii(Labels, lists:reverse(label_to_ascii(Label), [$.|Acc])).
 
 label_to_ascii(Label) ->
-  case lists:all(fun(C) -> idna_ucs:is_ascii(C) end, Label) of
-    true ->
-      Label;
-    false ->
-      ?ACE_PREFIX ++ punycode:encode(idna_unicode:normalize_kc(Label))
-  end.
+    case lists:all(fun(C) -> idna_ucs:is_ascii(C) end, Label) of
+        true ->
+            Label;
+        false ->
+            ?ACE_PREFIX ++ punycode:encode(idna_unicode:normalize_kc(Label))
+    end.
