0266d77532744095c0a6826a9639ac294f38aa9e
benoitc <bchesneau@gmail.com>
2014-04-14 02:25:15 +0200

    fix indent

--- src/idna_unicode.erl
+++ src/idna_unicode.erl
@@ -29,111 +29,111 @@
 %%============================================================================
 
 compose([]) ->
-  [];
+    [];
 compose([Starter|Unicode]) ->
-  StarterCC = case idna_unicode_data:combining_class(Starter) of 0 -> 0; _ -> 256 end,
-  compose(Starter, StarterCC, Unicode, []).
+    StarterCC = case idna_unicode_data:combining_class(Starter) of 0 -> 0; _ -> 256 end,
+    compose(Starter, StarterCC, Unicode, []).
 
 decompose(Unicode) ->
-  lists:reverse(lists:foldl(fun(CP, Acc) -> codepoint_decompose(CP, Acc) end, [], Unicode)).
+    lists:reverse(lists:foldl(fun(CP, Acc) -> codepoint_decompose(CP, Acc) end, [], Unicode)).
 
 downcase(Unicode) ->
-  [codepoint_downcase(CP) || CP <- Unicode].
+    [codepoint_downcase(CP) || CP <- Unicode].
 
 normalize_kc(Unicode) ->
-  compose(sort_canonical(decompose(Unicode))).
+    compose(sort_canonical(decompose(Unicode))).
 
 sort_canonical(Unicode) ->
-  Length = length(Unicode),
-  case Length < 2 of
-    true ->
-      Unicode;
-    false ->
-      sort_canonical(array:from_list(Unicode), 1, Length)
-  end.
+    Length = length(Unicode),
+    case Length < 2 of
+        true ->
+            Unicode;
+        false ->
+            sort_canonical(array:from_list(Unicode), 1, Length)
+    end.
 
 %%============================================================================
 %% Helper functions
 %%============================================================================
 
 compose(Starter, _, [], Acc) ->
-  lists:reverse([Starter|Acc]);
+    lists:reverse([Starter|Acc]);
 compose(Starter, StarterCC, [CP|Unicode], Acc) ->
-  Composite = compose_pair(Starter, CP),
-  case StarterCC =:= 0 andalso Composite =/= undefined of
-    true ->
-      compose(Composite, idna_unicode_data:combining_class(Composite), Unicode, Acc);
-    false ->
-      compose(CP, idna_unicode_data:combining_class(CP), Unicode, [Starter|Acc])
-  end.
+    Composite = compose_pair(Starter, CP),
+    case StarterCC =:= 0 andalso Composite =/= undefined of
+        true ->
+            compose(Composite, idna_unicode_data:combining_class(Composite), Unicode, Acc);
+        false ->
+            compose(CP, idna_unicode_data:combining_class(CP), Unicode, [Starter|Acc])
+    end.
 
 compose_pair(A, B) when
-  A >= ?HANGUL_LBASE andalso
-  A < (?HANGUL_LBASE + ?HANGUL_LCOUNT) andalso
-  B >= ?HANGUL_LBASE andalso
-  B < (?HANGUL_VBASE + ?HANGUL_VCOUNT) ->
-  ?HANGUL_SBASE + ((A - ?HANGUL_LBASE) * ?HANGUL_VCOUNT + (B - ?HANGUL_VBASE)) * ?HANGUL_TCOUNT;
+        A >= ?HANGUL_LBASE andalso
+        A < (?HANGUL_LBASE + ?HANGUL_LCOUNT) andalso
+        B >= ?HANGUL_LBASE andalso
+        B < (?HANGUL_VBASE + ?HANGUL_VCOUNT) ->
+    ?HANGUL_SBASE + ((A - ?HANGUL_LBASE) * ?HANGUL_VCOUNT + (B - ?HANGUL_VBASE)) * ?HANGUL_TCOUNT;
 compose_pair(A, B) when
-  A >= ?HANGUL_SBASE andalso
-  A < (?HANGUL_SBASE + ?HANGUL_SCOUNT) andalso
-  ((A - ?HANGUL_SBASE) rem ?HANGUL_TCOUNT) =:= 0 andalso
-  B >= ?HANGUL_TBASE andalso
-  B < (?HANGUL_TBASE + ?HANGUL_TCOUNT) ->
-  A + (B - ?HANGUL_TBASE);
+        A >= ?HANGUL_SBASE andalso
+        A < (?HANGUL_SBASE + ?HANGUL_SCOUNT) andalso
+        ((A - ?HANGUL_SBASE) rem ?HANGUL_TCOUNT) =:= 0 andalso
+        B >= ?HANGUL_TBASE andalso
+        B < (?HANGUL_TBASE + ?HANGUL_TCOUNT) ->
+    A + (B - ?HANGUL_TBASE);
 compose_pair(A, B) ->
-  idna_unicode_data:composition(A, B).
+    idna_unicode_data:composition(A, B).
 
 codepoint_decompose(CP, Acc) when CP >= ?HANGUL_SBASE andalso CP < (?HANGUL_SBASE + ?HANGUL_SCOUNT) ->
-  lists:reverse(decompose_hangul(CP), Acc);
+    lists:reverse(decompose_hangul(CP), Acc);
 codepoint_decompose(CP, Acc) ->
-  case idna_unicode_data:compat(CP) of
-    {error, bad_codepoint} ->
-      [CP|Acc];
-    undefined ->
-      [CP|Acc];
-    Codepoints ->
-      lists:reverse(decompose(Codepoints), Acc)
-  end.
+    case idna_unicode_data:compat(CP) of
+        {error, bad_codepoint} ->
+            [CP|Acc];
+        undefined ->
+            [CP|Acc];
+        Codepoints ->
+            lists:reverse(decompose(Codepoints), Acc)
+    end.
 
 decompose_hangul(CP) ->
-  Sindex = CP - ?HANGUL_SBASE,
-  case (Sindex < 0 orelse Sindex >= ?HANGUL_SCOUNT) of
-    true ->
-      [CP];
-    false ->
-      L = ?HANGUL_LBASE + Sindex div ?HANGUL_NCOUNT,
-      V = ?HANGUL_VBASE + (Sindex rem ?HANGUL_NCOUNT) div ?HANGUL_TCOUNT,
-      T = ?HANGUL_TBASE + Sindex rem ?HANGUL_TCOUNT,
-      case T =:= ?HANGUL_TBASE of
+    Sindex = CP - ?HANGUL_SBASE,
+    case (Sindex < 0 orelse Sindex >= ?HANGUL_SCOUNT) of
         true ->
-          [L, V];
+            [CP];
         false ->
-          [L, V, T]
-      end
-  end.
+            L = ?HANGUL_LBASE + Sindex div ?HANGUL_NCOUNT,
+            V = ?HANGUL_VBASE + (Sindex rem ?HANGUL_NCOUNT) div ?HANGUL_TCOUNT,
+            T = ?HANGUL_TBASE + Sindex rem ?HANGUL_TCOUNT,
+            case T =:= ?HANGUL_TBASE of
+                true ->
+                    [L, V];
+                false ->
+                    [L, V, T]
+            end
+    end.
 
 codepoint_downcase(CP) ->
-  case idna_unicode_data:lowercase(CP) of
-    {error, bad_codepoint} ->
-      CP;
-    Lowercase ->
-      Lowercase
-  end.
+    case idna_unicode_data:lowercase(CP) of
+        {error, bad_codepoint} ->
+            CP;
+        Lowercase ->
+            Lowercase
+    end.
 
 sort_canonical(Unicode, I, Length) ->
-  case I < Length of
-    true ->
-      array:to_list(Unicode);
-    false ->
-      Last = array:get(I - 1),
-      CP = array:get(I),
-      LastCC = idna_unicode_data:combining_class(Last),
-      CC = idna_unicode_data:combining_class(CP),
-      case CC =/= 0 andalso LastCC =/= 0 andalso LastCC > CC of
+    case I < Length of
         true ->
-          NextI = case I > 1 of true -> I - 1; false -> I end,
-          sort_canonical(array:set(I - 1, CP, array:set(I, Last, Unicode)), NextI, Length);
+            array:to_list(Unicode);
         false ->
-          sort_canonical(Unicode, I + 1, Length)
-      end
-  end.
+            Last = array:get(I - 1),
+            CP = array:get(I),
+            LastCC = idna_unicode_data:combining_class(Last),
+            CC = idna_unicode_data:combining_class(CP),
+            case CC =/= 0 andalso LastCC =/= 0 andalso LastCC > CC of
+                true ->
+                    NextI = case I > 1 of true -> I - 1; false -> I end,
+                    sort_canonical(array:set(I - 1, CP, array:set(I, Last, Unicode)), NextI, Length);
+                false ->
+                    sort_canonical(Unicode, I + 1, Length)
+            end
+    end.
