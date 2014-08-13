5089240e653ce254273da9cff57952a60b1d2c51
benoitc <bchesneau@gmail.com>
2014-08-13 16:53:39 +0200

    bump elixir packaging

--- package.exs
+++ package.exs
@@ -3,7 +3,7 @@ defmodule IDNA.Mixfile do
 
   def project do
     [app: :idna,
-     version: "1.0.0",
+     version: "1.0.1",
      description: "A pure Erlang IDNA implementation",
      package: package]
   end
